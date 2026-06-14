2616baceba4927108a5930514822d692683243f9
Arief Hidayat <82217590+arihid@users.noreply.github.com>
2026-06-14 17:23:18 +0900

    Fixed dependency error
    Fixed dependency error

--- omni_kyf42.mk
+++ omni_kyf42.mk
@@ -1,5 +1,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, vendor/twrp/config/twimage.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_DEVICE := kyf42
 PRODUCT_NAME := omni_kyf42
